08d414e77ff9fb4872bf4d54fa75196f61c3abe3
Vishal Sharma <785vishal443@gmail.com>
2023-12-07 22:35:44 +0530

    useRef uses=>!

--- src/component/RenderExample.jsx
+++ src/component/RenderExample.jsx
@@ -1,21 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const RenderExample = ()=> {
-    let b=30;
-    console.log('Hey I am rende111' ,b )
+    let b = useRef(30); 
+    let a = 60;
+    console.log('b=>' ,b )
     const [headerText , setHeaderText] = useState('Hey this is rendering exmaple');
     const  [subHeaderText , setSubHeaderText]= useState('Hey this is sub header text!!!!');
     const handleClick =(type)=>{
         if(type == 'subheader') {
             setSubHeaderText('Hurrah Sub HEADFEER CHANGFED')
-            b=80;
+            // b.current=80;
+            a= 100;
         } else {
-            b=90;
+            b.current=90;
+            a= 200
             setHeaderText('Hurrah!!!! Changed Header Text')
         }
     }
 
-    console.log('Hey I am render2xs' , b)
+    console.log('last  a=>' , a)
+    console.log(' last  b=>' , b)
     return  (
         <div>
           <h2>{headerText}</h2> 
@@ -34,6 +38,8 @@ export default RenderExample;
 
 
 
+// count how many time function get call ()
+
 // re-render(calling the function again) , render(first time calling the function)
 
 // render => in this case all your state and all the variable related to the component is intial initialzed;
@@ -43,3 +49,17 @@ export default RenderExample;
 // in the case of re-render and render in this both cases normal variable always re-intialzed
 
 
+//I have some rquirement from react that , i want create one varibale which will behave like below mentioned point
+
+// This will initialzed only at the mounting time(element dom eneter  or render or fist func call);
+// this will not re-initialzed at re-rendering
+// it will persist the previous data like state but it should not cause for re-rendering the component
+
+
+
+
+// ref ?? 
+// every element have ref attribute 
+
+// useRef 
+// Persist the data without re-rendering (usePrevious)
