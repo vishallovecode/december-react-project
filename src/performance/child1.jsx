43e3bd156b35aa0ed1edd56e1c7bcb9465a2f9e2
Vishal Sharma <785vishal443@gmail.com>
2023-12-12 22:43:19 +0530

    usecallback

--- src/performance/child1.jsx
+++ src/performance/child1.jsx
@@ -1,9 +1,14 @@
 import { memo } from "react";
 
 const Child1 = (props)=>{
+    const  {onClick ,heading} = props;
     console.log('Child1')
     return  (
-        <h1>{props.heading}</h1>
+        <>
+        <h1>{heading}</h1>
+        <button onClick = {()=>onClick('Updated Child1')}>Change child2</button>
+        </>
+
     )
 }
 
--- src/performance/performance.jsx
+++ src/performance/performance.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Child1 from "./child1";
 import Child2 from "./child2";
 import Child3 from "./child3";
@@ -10,14 +10,29 @@ const Performance = ()=> {
     const [child2 , setChild2] = useState('Child2')
     const [child3 , setChild3] = useState('Child3')
     console.log('Parent')
+
+
+    const handlChild1 = ()=> {
+        setChild1('Updated Child1');
+    }
+
+   const func =  useCallback(()=>{
+           handlChild1()
+           // this code will be run only once after mounting
+           // here you can pass any dependency 
+           // whenever that dependency change this function will recreated and reference will change
+    } , [child3])
+
+
     return (
         <>
        <h2>{pHeading}</h2>
        <button onClick = {()=>setPHeading('Updated The heading')}>Change</button>
-       <button onClick = {()=>setChild1('Updated Child1')}>Change child2</button>
+       {/* <button onClick = {()=>setChild1('Updated Child1')}>Change child2</button>
+       */}
        <button onClick = {()=>setChild2('Updated Child12')}>Change child2</button>
-       <button onClick = {()=>setChild3('Updated Child13')}>Change child3</button>
-       <Child1 heading = {child1}/>
+       <button onClick = {()=>setChild3('Updated Child13')}>Change child3</button> 
+       <Child1 heading = {child1} onClick = {func}/>
        <Child2 heading = {child2}/>
        <Child3 heading = {child3}/>
      </>
@@ -26,3 +41,5 @@ const Performance = ()=> {
 
 
 export default Performance;
+
+// useCallbackn returns the meoized functions
